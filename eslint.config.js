146c11957c1c824d0c3260738c3ac347a35cde4f
vikraj66 <vikasraj041013@gmail.com>
2024-06-05 09:38:20 +0530

    fix: no liniting for now

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
-// @ts-check
-
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+    rules: {
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+    },
+  },
+];
